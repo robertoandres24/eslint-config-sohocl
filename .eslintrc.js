e54f29ebd1699890ecde41c9894a6b8a6f2aa75e
roberto <robertoandres24@gmail.com>
2020-01-26 16:52:38 -0300

    update readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,16 @@ module.exports = {
     "plugin:prettier/recommended"
   ],
   rules: {
-    "vue/component-name-in-template-casing": ["error", "PascalCase"],
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "warning",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warning",
+    "prettier/prettier": [
+      "error",
+      {
+        "singleQuote": true,
+        "printWidth": 80,
+        "semi": false
+      }
+    ]
   },
   globals: {
     $nuxt: true
